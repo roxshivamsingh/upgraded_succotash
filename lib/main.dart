389390d3e7dddf24a79dcad73ca66a8d06c2bab1
Shivam Singh <roxshivamsingh@gmail.com>
2025-05-24 19:25:43 +0530

    ADD: Component > Drawer - component;

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,44 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+
+            return IconButton(
+              padding: EdgeInsets.all(10),
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+              onLongPress: (){
+                Scaffold.of(context).openDrawer();
+
+              },
+            );
+          },
+        ),
       ),
+      drawer: Drawer(child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Welcome, Shivam Singh')),
+          ListTile(
+            title: const Text('Home'),
+            onTap: () {
+
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text('About'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
